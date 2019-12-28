3fee81600436a8385ea3ba044d91fbbb8669bcaf
gaspardmonet <marianoparra1118@gmail.com>
2019-12-28 06:46:27 +0500

    updated for parsing number

--- with-fulfillment-library/functions/index.js
+++ with-fulfillment-library/functions/index.js
@@ -17,43 +17,36 @@ exports.webhook = functions.https.onRequest((request, response) => {
 
         let params = agent.parameters;
         let operation = agent.parameters['operation'];
-        let number1 = agent.parameters['number1'];
-        let number2 = agent.parameters['number2'];
+        let number1 = parseFloat(agent.parameters['number1']);
+        let number2 = parseFloat(agent.parameters['number2']);
 
         switch (params.operation) {
             case "Addition":
                 agent.add(` The ${operation} of ${number1}
-                and ${number2} is ${number1 + number2} `
-                );
+                and ${number2} is ${number1 + number2} `);
                 break;
 
 
             case "Subtraction":
                 agent.add(` The ${operation} of ${number1}
-                and ${number2} is ${number1 - number2} `
-                );
+                and ${number2} is ${number1 - number2} `);
                 break;
 
             case "Multiplication":
                 agent.add(` The ${operation} of ${number1}
-                and ${number2} is ${number1 * number2} `
-                );
+                and ${number2} is ${number1 * number2} `);
                 break;
 
             case "Division":
                 if (params.number2 !== 0) {
                     agent.add(` The ${operation} of ${number1}
-                    and ${number2} is ${number1 / number2} `
-                    );
-                }
-                else {
-                    agent.add(`Sorry, this calculator can't perform calculation of divident with 0 `
-                    );
+                    and ${number2} is ${number1 / number2} `);
+                } else {
+                    agent.add(`Sorry, this calculator can't perform calculation of divident with 0 `);
                 }
                 break;
             default:
-                agent.add('Sorry we only can only perform ADDITION SUBTRACTION MULTIPLICATION AND DIVISION'
-                );
+                agent.add('Sorry we only can only perform ADDITION SUBTRACTION MULTIPLICATION AND DIVISION');
         }
 
     }
